458d9bbaf8ba38a5c0b48b572a7b55b56a046663
Austyn Trace <atrace@gmail.com>
2017-09-04 13:53:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(){
 function increaseRankBy(n){
   var ranks = document.querySelectorAll('.ranked-list li');
   for(let i=0;i<ranks.length; i++){
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + parseInt(n);
   }
 }
